710566a3165aaf6fa3804ea1e55a87b27ac41ed2
iStagnant <eliasghafari@disroot.org>
2024-03-02 23:19:43 +0200

    Removed unnecessary files and configured opts

--- lua/options.lua
+++ lua/options.lua
@@ -1,13 +1,10 @@
 -- [[ Setting options ]]
 -- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
+-- Make relative line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, for help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -16,7 +13,6 @@ vim.opt.mouse = 'a'
 vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
@@ -37,15 +33,30 @@ vim.opt.signcolumn = 'yes'
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = 'menuone,noselect'
+
 -- Configure how new splits should be opened
-vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+-- Enable 24-bit colors
+vim.opt.termguicolors = true
 
 -- Sets how neovim will display certain whitespace in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Make tabs expand to spaces
+vim.opt.expandtab = true
+
+-- Set default tab size
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Allow selection of empty cells in visual block mode
+vim.opt.virtualedit = "block"
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
